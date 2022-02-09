3426d9e88d750dfe27d305ab7e035a0d553fb706
zbmalak <zbigniew.malak@gmail.com>
2022-02-09 20:47:08 +0100

    update

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-"use strict";
+'use strict';
 
-const modalContainer = document.querySelector(".modal--container");
+const modalContainer = document.querySelector('.modal--container');
 
-const fictionContainer = document.querySelector(".movies--fiction");
-const documentaryContainer = document.querySelector(".movies--documentary");
-const commercialContainer = document.querySelector(".movies--commercial");
-const personalContainer = document.querySelector(".movies--personal");
-const musicvideoContainer = document.querySelector(".movies--musicvideo");
+const fictionContainer = document.querySelector('.movies--fiction');
+const documentaryContainer = document.querySelector('.movies--documentary');
+const commercialContainer = document.querySelector('.movies--commercial');
+const personalContainer = document.querySelector('.movies--personal');
+const musicvideoContainer = document.querySelector('.movies--musicvideo');
 
-document.querySelector(".nav__links").addEventListener("click", function (e) {
-  if (e.target.classList.contains("nav__link")) {
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  if (e.target.classList.contains('nav__link')) {
     e.preventDefault();
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
 const movies = {
-  fictionMovies: ["spN17NY6VlU", "h_Z0z5aBh7g", "dMdJUfna1SI", "QiqH5ym7ZuM"],
-  documentaryMovies: ["cJSCh2U_uNY", "Tsm_PdFATfE"],
-  musicvideoMovies: ["7BKIfWBqjrs", "suvfh2D_y_4", "FlHacOV0tg4"],
-  commercialMovies: ["ZKfb3-NFAdk", "_HvKe_e1JOQ", "4e7cQ281NQw"],
+  fictionMovies: ['spN17NY6VlU', 'h_Z0z5aBh7g', 'dMdJUfna1SI', 'QiqH5ym7ZuM'],
+  documentaryMovies: ['cJSCh2U_uNY', 'Tsm_PdFATfE'],
+  musicvideoMovies: [
+    '7BKIfWBqjrs',
+    'suvfh2D_y_4',
+    'FlHacOV0tg4',
+    'LbJ-q6Nr974',
+  ],
+  commercialMovies: ['ZKfb3-NFAdk', '_HvKe_e1JOQ', '4e7cQ281NQw'],
   personalMovies: [
-    "MgU4UrTDkRc",
-    "rvBULEFohNs",
-    "aM7SxNE13TI",
-    "Dkjq4QsJuAg",
-    "WLDDAHMTIUg",
-    "V1_7aIxYXJ8",
-    "6UAi8fgAZtY",
+    'MgU4UrTDkRc',
+    'rvBULEFohNs',
+    'aM7SxNE13TI',
+    'Dkjq4QsJuAg',
+    'WLDDAHMTIUg',
+    'V1_7aIxYXJ8',
+    '6UAi8fgAZtY',
   ],
 };
 
@@ -40,7 +45,7 @@ const renderMovies = function (arr, container) {
         src="https://i3.ytimg.com/vi/${movieID}/hqdefault.jpg"
       />
      `;
-    container.insertAdjacentHTML("afterbegin", markup);
+    container.insertAdjacentHTML('afterbegin', markup);
 
     const modalHtml = `
       <div class="modal">
@@ -60,8 +65,8 @@ const renderMovies = function (arr, container) {
 
     document
       .getElementById(`${movieID}`)
-      .addEventListener("click", function () {
-        modalContainer.insertAdjacentHTML("afterbegin", modalHtml);
+      .addEventListener('click', function () {
+        modalContainer.insertAdjacentHTML('afterbegin', modalHtml);
       });
   });
 };
@@ -72,8 +77,8 @@ renderMovies(movies.musicvideoMovies, musicvideoContainer);
 renderMovies(movies.commercialMovies, commercialContainer);
 renderMovies(movies.personalMovies, personalContainer);
 
-document.addEventListener("click", function (e) {
-  if (e.target && e.target.classList == "close--modal") {
-    modalContainer.innerHTML = "";
+document.addEventListener('click', function (e) {
+  if (e.target && e.target.classList == 'close--modal') {
+    modalContainer.innerHTML = '';
   }
 });
